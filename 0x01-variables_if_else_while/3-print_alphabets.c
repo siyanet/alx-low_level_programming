07725e6d516d2339a622d082bc89657bfb15b396
siyanet <siyangetahunw@gmail.com>
2022-10-14 12:32:53 -0700

    codes are added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Entry
+  *Return: Always 0
+  */
+int main(void)
+{
+	for (int d = 65; d <= 90; d++)
+	{
+		char c = (char)d;
+
+		putchar(c);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
