ec54e6fbac86ad3f116b177e702fad912e9ddaaa
siyanet <siyangetahunw@gmail.com>
2022-10-14 07:26:53 -0700

    scipt added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+  *main - Entry
+  *Return: Always 0
+  */
 int main(void)
 {
-	int i = sizeof(int);
-	int f = sizeof(float);
-	int c = sizeof(char);
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of a int: %ld byte(s)\n", sizeof(int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	return (0);
 }
