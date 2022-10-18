8840a6a417aa9d0d0451257345776ad1e6d3fddd
siyanet <siyangetahunw@gmail.com>
2022-10-18 12:48:40 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,6 @@
 int _putchar(void)
 {
-	putchar('_');
-
-	putchar('p');
-
-	putchar('u');
-
-	putchar('t');
-
-	putchar('c');
-
-	putchar('h');
-
-	putchar('a');
-
-	putchar('r');
-
-	putchar('\n');
+	write("_putchar\n");
 
 	return (0);
 }
