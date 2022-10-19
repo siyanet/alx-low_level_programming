08670fef979709b41ebdc672bf11f7e8662c07fc
siyanet <siyangetahunw@gmail.com>
2022-10-19 09:07:14 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,13 @@
+#include "main.h"
+/**
+  *print_alphabet - to print small letter alphabet
+  *Return: 0
+  */
+
 void print_alphabet(void)
 {
 	char i;
-	for(i='a'; i<='z'; i++)
+	for(i = 'a'; i <= 'z'; i++)
 	{
 		_putchar('i');
 
