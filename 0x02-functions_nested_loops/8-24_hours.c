7d756a7c1deabd5206b7d462eefcb96fa0e2347f
siyanet <siyangetahunw@gmail.com>
2022-10-19 16:09:29 -0700

    update'

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,12 +9,16 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
+					if (i == 2 && j > 3)
+					{
+						break;
+					}
 					_putchar(i + '0');
 
 					_putchar(j + '0');
