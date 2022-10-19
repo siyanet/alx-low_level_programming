e51dcdd27832b5fcfbf837f223c3b7b0e7cd553d
siyanet <siyangetahunw@gmail.com>
2022-10-19 14:00:44 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
   *_isalpha -return 1 if the letter is lower or return  if it is not
-  *@c: the character that will be checked 
+  * @c: the character that will be checked 
   *Return: sucess 1 failer 0
   */
 int _isalpha(int c)
