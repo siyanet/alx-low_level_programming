519d486b27147329ffacd6784678ef8ec86b3b35
siyanet <siyangetahunw@gmail.com>
2022-10-20 14:28:18 -0700

    fixing 5-more_functions.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,17 +12,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j <= 9)
-			{
-				_putchar(j + '0');
-			}
-			else
+			if (j > 9)
 			{
 				_putchar(1 + '0');
 
-				_putchar(j % 10 + '0');
-
 			}
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 
