6af84505f85751843f593602e3e7f724d719a89e
siyanet <siyangetahunw@gmail.com>
2022-10-26 15:43:23 -0700

    main.h updated

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,4 +3,8 @@
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char _putchar(char s);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *string_toupper(char *);
+char *cap_string(char *);
 #endif
