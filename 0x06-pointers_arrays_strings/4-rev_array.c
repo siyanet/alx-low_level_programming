a14585ea2c8e5adb57a4a876700b854bf2cc25a5
siyanet <siyangetahunw@gmail.com>
2022-10-26 15:26:49 -0700

    code added

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *reverse_array - make array in reverse order
+  *@a: array
+  *@n: array size
+  */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	int *t = a;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		a++;
+
+	}
+	for (i = 0; i < ((n - 1) / 2); i++)
+	{
+		*a = t[i];
+
+		a[i] = t[n - 1 - i];
+
+		a--;
+
+		t++;
+	}
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *string_toupper - change strings to upper case
+  *@s: character pointer
+  *Return: pointer
+  */
+char *string_toupper(char *s)
+{
+	char *s1 = s;
+
+	if (*s != '\0' && (*s >= 65 && *s <= 90))
+	{
+		s++;
+
+	}
+	else if (*s != '\0' && *s >= 97 && *s <= 122)
+	{
+		*s = *s - 32;
+
+		s++;
+
+	}
+	return (s1);
+}
