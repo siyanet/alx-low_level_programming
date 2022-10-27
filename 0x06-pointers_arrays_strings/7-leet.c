2f5b8f554bc0c3432d7f8ffef2574c146a116923
siyanet <siyangetahunw@gmail.com>
2022-10-27 10:30:18 -0700

    mandantory finished

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  *leet - change to 1337
+  *@s: string
+  *Return: pointer
+  */
+char *leet(char *s)
+{
+	int i, j;
+
+	char a[] = "aAeEoOtTlL";
+
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) == b[j];
+
+			}
+		}
+	}
+	return (s);
+}
