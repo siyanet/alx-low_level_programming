f9cecac7a10dedc44d2b93e0cf7f7431884b3d42
siyanet <siyangetahunw@gmail.com>
2022-10-27 08:43:39 -0700

    task 6 added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+  *cap_string - change strings to upper case
+  *@s: character pointer
+  *Return: pointer
+  */
+char *cap_string(char *s)
+{
+	char *s1 = s;
+
+	while (*s != '\0')
+	{
+		if (*s == '\n' || *s == '\t' || *s == ' ' || *s ==  ',' || *s == ';' ||
+				*s == '.' || *s == '!' || *s == '?' || *s ==  '"' ||
+				*s == '(' || *s == ')' || *s == '{' || *s == '}')
+		{
+			s++;
+
+			if (*s != '\0' && (*s >= 'a'  && *s <= 'z'))
+			{
+				*s = *s - 32;
+
+				s++;
+
+			}
+			else
+			{
+				s++;
+			}
+		}
+		else
+		{
+			s++;
+
+		}
+	}
+	return (s1);
+}
