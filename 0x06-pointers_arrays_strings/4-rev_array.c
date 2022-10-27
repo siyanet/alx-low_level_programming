b1c17c7aa3eeaafef434d03d942ac4a64d89ac2c
siyanet <siyangetahunw@gmail.com>
2022-10-27 06:50:55 -0700

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,15 +7,17 @@
   */
 void reverse_array(int *a, int n)
 {
-	int i;
-
-	int *t = a;
+	int i, c, d;
 
 	for (i = 0; i < ((n - 1) / 2); i++)
 	{
-		*(a + n - 1 - i) = *(t + i);
+		c = a[i];
 
-		*(a + i) = *(t - n - 1 - i);
+		d = a[n - 1 - i];
+
+		a[n - 1 - i] = c;
+
+		a[i] = d;
 
 	}
 }
