39f14b0ef60ffc07974870ec190d2b4c4431b832
siyanet <siyangetahunw@gmail.com>
2022-10-28 04:43:45 -0700

    add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *main -prits to buffer
+  *Return: 1 always
+  */
+int main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+			59);
+
+	return (1);
+}
