fd2bc568c62a429f7d2651f8d9958cde64abcab3
siyanet <siyangetahunw@gmail.com>
2022-11-03 03:51:20 -0700

    6-is_prime_number.c is added

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+  *check -check if the number is prime or not
+  *@a: divisor
+  *@n: number to be checked
+  *Return: 1 or 0;
+  */
+int check(int a, int n)
+{
+	if (a < n)
+	{
+		if (n % a == 0)
+		{
+			return (0);
+		}
+		return (check(a + 1, n));
+	}
+	else
+	{
+		return (1);
+	}
+}
+/**
+  *is_prime_number - prints prime number
+  *@n: number to be checked
+  *Return: 0 if not prime 1 if prime
+  */
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		n = -1 * n;
+
+	}
+	return (check(2, n));
+}
