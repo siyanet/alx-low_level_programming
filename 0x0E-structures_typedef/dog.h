f422236356f3a2363a20142317a5e0b165249761
siyanet <siyangetahunw@gmail.com>
2022-11-14 20:14:00 +0000

    debugging dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
+
 /**
- * sturnct dog - dog struct
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
+  *sturnct dog - dog struct
+  *@name: name of the dog
+  *@age: age of the dog
+  *@owner: owner of the dog
+  */
 struct dog
 {
 	char *name;
@@ -13,10 +14,17 @@ struct dog
 	char *owner;
 };
 void print_dog(struct dog *d);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+/**
+  *dog_t -Typedef for dog
+  */
 typedef struct dog dog_t;
+
 void free_dog(dog_t *d);
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
 
