be47d1ac0873966c406294a793027fcfdd36afac
siyanet <siyangetahunw@gmail.com>
2022-11-14 18:37:16 +0000

    dog.h added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * sturnct dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
