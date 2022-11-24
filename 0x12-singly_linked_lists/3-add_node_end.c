cb1bf12a76e041f4e4934d637dcb94fdc9f98f90
siyanet <siyangetahunw@gmail.com>
2022-11-24 18:23:29 +0000

    code added

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+  *_strlen -finds length
+  *@str: string
+  *@Return: length of string
+  */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+/**
+  *add_node_end  -adds a new node to the end of linked list
+  *@head: double pointer to linked list
+  *@str: string 
+  *Return: pointer
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+
+	if (str == NULL)
+		return (NULL);
+	new = mallo(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str == strdup(str);
+	if (new-> == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
+	return (new);
+}
